227b662bf60a9a21dda11862ebb17a7511c8e73f
Shikhar Ambashta <shikhar.k.a@gmail.com>
2019-03-11 20:49:46 +0530

    Dropdown Select + Background

--- src/app/matchdetails/matchdetails.component.ts
+++ src/app/matchdetails/matchdetails.component.ts
@@ -7,19 +7,28 @@ import { MatchdataService } from '../matchdata.service';
   styleUrls: ['./matchdetails.component.css']
 })
 export class MatchdetailsComponent implements OnInit {
-  matchdetails: any;
+  matchdetails: any = [];
   monthNames = ["Jan", "Feb", "Mar", "Apr", "May", "Jun",
     "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"
   ];
+  currentMatchday = '';
+  currenMatchdayName = '';
 
   constructor(private matchdata: MatchdataService) { }
 
   ngOnInit() {
     this.matchdata.getJSON().subscribe(data => {
       this.matchdetails = data
+      // this.currentMatchday = this.matchdetails.rounds[0];
+      this.currenMatchdayName = this.matchdetails.rounds[0].name
+      this.setMatchDayDetails(this.currenMatchdayName)
       this.matchdetails.rounds.map(round => {
         round.matches.map(match => match.date = new Date(match.date))
       })
     })
+  }
+
+  setMatchDayDetails = (matchday) => {
+    this.currentMatchday = this.matchdetails.rounds.find(elem => elem.name === matchday)
   }
 }
