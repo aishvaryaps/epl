c50f31105aed21a1f7af9929b2275cf05a74e8e9
Shikhar Ambashta <shikhar.k.a@gmail.com>
2019-04-13 15:09:40 +0530

    Search optimised

--- src/app/team-search.pipe.ts
+++ src/app/team-search.pipe.ts
@@ -6,12 +6,12 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class TeamSearchPipe implements PipeTransform {
 
   transform(value: any, args?: any): any {
-    if(args !== ""){
-      return value.filter(team => {
-        return team.name.toLowerCase().includes(args.toLowerCase())
-      });
+
+    if (args !== "") {
+      let teamRegex = new RegExp('^' + args, 'i')
+      return value.filter(team => teamRegex.test(team.name));
     }
-    else{
+    else {
       return value
     }
   }
